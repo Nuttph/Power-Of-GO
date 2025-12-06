0e1f1acccb671283ac282bbb2e2b0237ecc0fd11
Nuttaphon <nathn2420@gmail.com>
2025-12-06 19:14:10 +0700

    connect database

--- main.go
+++ main.go
@@ -1,7 +1,59 @@
 package main
 
-import "fmt"
+import (
+	"database/sql"
+	"fmt"
+	"log"
+
+	_ "github.com/lib/pq"
+)
+
+const (
+	host         = "localhost"
+	port         = 5432
+	databaseName = "mydatabase"
+	username     = "myuser"
+	password     = "mypassword"
+)
+
+var db *sql.DB
+
+type Product struct {
+	ID    int
+	Name  string
+	Price int
+}
 
 func main() {
-	fmt.Println("Hello, World!")
+	psqlInfo := fmt.Sprintf("host=%s port=%d user=%s password=%s dbname=%s sslmode=disable", host, port, username, password, databaseName)
+
+	sdb, err := sql.Open("postgres", psqlInfo)
+	if err != nil {
+		log.Fatal(err)
+	}
+	err = sdb.Ping()
+	if err != nil {
+		log.Fatal(err)
+	}
+	db = sdb
+
+	println("Successfully connected to the database!")
+
+	err = createProduct(&Product{Name: "Go Product", Price: 222})
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	print("Successfully created product!")
+
+}
+
+func createProduct(product *Product) error {
+	// Insert
+
+	_, err := db.Exec(
+		"INSERT INTO public.products(name, price) VALUES ($1, $2);",
+		product.Name, product.Price)
+
+	return err
 }
